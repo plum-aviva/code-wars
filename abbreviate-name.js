97f8e6822764637adf21ceffeb2cca533729d233
Plum Bristow <plum.bristow@aviva.com>
2025-06-22 09:29:38 +0100

    adding abbreviated initial code wars

--- abbreviate-name.js
+++ abbreviate-name.js
@@ -1,9 +1,17 @@
 function abbrevName(name) {
+    let splitName = name.split(" ");
+    let firstName = splitName[0];
+    let firstInitial = firstName[0];
+    let secondName = splitName[1];
+    let secondInitial = secondName[0]
+    let answer = `${firstInitial}.${secondInitial}`
+    return answer.toUpperCase();
 
 
-
-    // code away
 
 }
 
-// 
+console.log(abbrevName("plum Bristow"))
+// Daniel Bristow = D.B
+// Plum Bristow
+
